91e5823ae4e63bbb687d9f278f53bcc730a717d2
NPEMasa <npemasa@outlook.jp>
2021-01-03 15:46:02 +0900

    modifyed form design.

--- src/result.php
+++ src/result.php
@@ -83,89 +83,37 @@ if(isset($_POST['thisAmount']) && isset($_POST['lastAmount']) && isset($_POST['a
         <h2>手取り計算ツール</h2>
       <div class="row">
           <div class="col-md-6">
-            <div class="card mb-2 shadow">
+            <div class="card border-primary mb-2 shadow">
               <div class="card-header">
                 年収総額
               </div>
-              <div class="card-body">
-                <?php if(isset($thisAmount)){ echo '¥' . number_format($thisAmount); } ?>
+              <div class="card-body text-primary">
+                <p>今年の年収：<?php if(isset($thisAmount)){ echo '¥' . number_format($thisAmount); } ?></p>
+                <p>去年の年収：<?php if(isset($lastAmount)){ echo '¥' . number_format($lastAmount); } ?></p>
               </div>
             </div>
           </div>
 
           <div class="col-md-6">
-            <div class="card mb-2 shadow">
+            <div class="card border-success mb-2 shadow">
               <div class="card-header">
-                前年年収総額
+                手取り
               </div>
-              <div class="card-body">
-                <?php if(isset($lastAmount)){ echo '¥' . number_format($lastAmount); } ?>
+              <div class="card-body text-success">
+                <p>手取り年収：<?php if(isset($totalYamount)){ echo '¥' . number_format($totalYamount); } ?></p>
+                <p>手取り月収：<?php if(isset($totalMamount)){ echo '¥' . number_format($totalMamount); } ?></p>
               </div>
             </div>
           </div>
 
           <div class="col-md-6">
-            <div class="card mb-2 shadow">
+            <div class="card border-danger mb-2 shadow">
               <div class="card-header">
-                年齢
+                税金
               </div>
-              <div class="card-body">
-                <?php if(isset($age)){ echo $age . '歳'; } ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <div class="card mb-2 shadow">
-              <div class="card-header">
-                所得控除額
-              </div>
-              <div class="card-body">
-                <?php if(isset($incomeDeduction)){ echo '¥' . number_format($incomeDeduction); } ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <div class="card mb-2 shadow">
-              <div class="card-header">
-                所得税
-              </div>
-              <div class="card-body">
-                <?php if(isset($incomeTax)){ echo '¥' . number_format($incomeTax); } ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <div class="card mb-2 shadow">
-              <div class="card-header">
-                住民税
-              </div>
-              <div class="card-body">
-                <?php if(isset($residentTax)){ echo '¥' . number_format($residentTax); } ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <div class="card mb-2 shadow">
-              <div class="card-header">
-                手取り年収
-              </div>
-              <div class="card-body">
-                <?php if(isset($totalYamount)){ echo '¥' . number_format($totalYamount); } ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <div class="card mb-2 shadow">
-              <div class="card-header">
-                手取り月収
-              </div>
-              <div class="card-body">
-                <?php if(isset($totalMamount)){ echo '¥' . number_format($totalMamount); } ?>
+              <div class="card-body text-danger">
+                <p>所得税：<?php if(isset($incomeTax)){ echo '¥' . number_format($incomeTax); } ?></p>
+                <p>住民税：<?php if(isset($residentTax)){ echo '¥' . number_format($residentTax); } ?></p>
               </div>
             </div>
           </div>
